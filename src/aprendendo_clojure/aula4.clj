7823e08a252e041bf2bd558e5f288644682601b9
AlanNunes <alann.625@gmail.com>
2021-07-06 13:00:19 -0300

    Map e Filter

--- src/aprendendo_clojure/aula4.clj
+++ src/aprendendo_clojure/aula4.clj
@@ -6,6 +6,25 @@
   [valor]
   (* valor 2))
 
-(println (precos 0))
-(println (update precos 0 dobra-valor))
-(println (update precos 0 dobra-valor))
+;(println (precos 0))
+;(println (update precos 0 dobra-valor))
+;(println (update precos 0 dobra-valor))
+
+;(println (map dobra-valor precos))
+
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se deve aplicar."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-desconto)]
+      (- valor-bruto desconto))
+      valor-bruto))
+
+;(println (map valor-descontado precos))
+(println "Preços que devem receber desconto:" (filter aplica-desconto? precos))
+(println "Preços com valor descontado:" (map valor-descontado (filter aplica-desconto? precos)))
