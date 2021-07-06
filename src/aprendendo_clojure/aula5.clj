892d04ee359698829d3b91433707405a5197d4c8
AlanNunes <alann.625@gmail.com>
2021-07-06 14:42:05 -0300

    Update-in e Threading First

--- src/aprendendo_clojure/aula5.clj
+++ src/aprendendo_clojure/aula5.clj
@@ -7,3 +7,16 @@
 (println (update estoque :Camisa dec))
 (println (keys estoque))
 (println (vals estoque))
+
+
+(def pedido {:mochila {:quantidade 10, :preco 40}
+             :cadeira {:quantidade 5, :preco 100}})
+
+(println "Preço da mochila do pedido:" (:preco (:mochila pedido)))
+(println "Quantidade da mochila do pedido:" (:quantidade (:mochila pedido)))
+(println "Estoque depois de atualizado:" (update-in pedido [:cadeira :quantidade] dec))
+
+; Threading First
+(println "Quantidade de mochilas usando Threading First:" (-> pedido
+             :mochila
+             :quantidade))
