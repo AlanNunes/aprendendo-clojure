33acbff7db335d892588d707011f12942e8b9530
AlanNunes <alann.625@gmail.com>
2021-07-22 23:24:25 -0300

    future

--- hospital/src/hospital/aula6.clj
+++ hospital/src/hospital/aula6.clj
@@ -2,8 +2,16 @@
   (:use [clojure pprint])
   (:require [hospital.model :as h.model]))
 
+(defn cabe-na-fila?
+  [fila]
+  (-> fila
+      (count)
+      (< 5)))
+
 (defn chega-em [fila pessoa]
-  (conj fila pessoa)
+  (if (cabe-na-fila? fila)
+    (conj fila pessoa)
+    (throw (ex-info "Fila já está cheia!" {:tentando-adicionar pessoa})))
   )
 (defn chega-em!
   [hospital pessoa]
@@ -22,8 +30,37 @@
                   :laboratorio2 (ref h.model/fila-vazia)
                   :laboratorio3 (ref h.model/fila-vazia)}]
     (dosync
-      (chega-em! hospital "Alan"))
+      (chega-em! hospital "Alan")
+      (chega-em! hospital "Karynne")
+      (chega-em! hospital "Tainan")
+      (chega-em! hospital "Luan")
+      (chega-em! hospital "Pretinha")
+      (chega-em! hospital "Black")
+      )
     (pprint hospital)
     ))
 
-(simula-um-dia)
+;(simula-um-dia)
+
+(defn async-chega-em! [hospital pessoa]
+  (future
+    (Thread/sleep (rand 5000))
+    (dosync
+      (println "Tentando o codigo sincronizado" pessoa)
+      (chega-em! hospital pessoa))))
+
+(defn simula-um-dia-async []
+  (let [hospital {:espera       (ref h.model/fila-vazia)
+                  :laboratorio1 (ref h.model/fila-vazia)
+                  :laboratorio2 (ref h.model/fila-vazia)
+                  :laboratorio3 (ref h.model/fila-vazia)}]
+    (dotimes [n 6]
+      (async-chega-em! hospital n))
+    (future
+      (dotimes [n 4]
+        (Thread/sleep 2000)
+        (pprint hospital)))
+    ))
+
+(simula-um-dia-async)
+;(println (future 15))
